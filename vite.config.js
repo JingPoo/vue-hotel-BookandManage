3a42e755adb909f7d07881f6d9a8123542d2503f
JingPoo <jingh861219@gmail.com>
2023-06-26 13:06:30 +0800

    edit config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-hotel-BookandManage/',
   plugins: [vue()],
   resolve: {
     alias: {
